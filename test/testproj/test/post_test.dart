97e033c012abf6bd3e6ca6fa9080073dde6673b9
Frannie <frannie.richert@gmail.com>
2021-03-11 12:36:05 -0500

    added a sample unit test file

---
+++ test/testproj/test/post_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+//import 'package:testproj/post.dart';
+
+void main(List<String> args) {
+  test('Post Created from Map should have approrpiate property values', (){
+
+
+
+  });
+}
